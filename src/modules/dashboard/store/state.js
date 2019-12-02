970257f7a1e8c10b9598ae9d77607464d20f27d0
Arman-Ahmed-Jony <armanahmedjony786@gmail.com>
2019-12-02 20:02:30 +0600

    commnent duplicate states

--- src/modules/dashboard/store/state.js
+++ src/modules/dashboard/store/state.js
@@ -1,13 +1,13 @@
-export default {
-  auth: {
-    isLoggedIn: false,
-    accessToken: null,
-  },
-  oauthclients: [],
-  user: {
-    first_name: null,
-    last_name: null,
-  },
-  loadingAuth: false,
+// export default {
+//   auth: {
+//     isLoggedIn: false,
+//     accessToken: null,
+//   },
+//   oauthclients: [],
+//   user: {
+//     first_name: null,
+//     last_name: null,
+//   },
+//   loadingAuth: false,
 
-};
+// };
